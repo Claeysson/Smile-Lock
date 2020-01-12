b6a151f94d1bd41ab491a2a2c1b15095670a3e82
Martin Claesson <43297668+Claeysson@users.noreply.github.com>
2020-01-12 22:35:04 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SmileLock",
-            dependencies: []),
-            path: "SmileLock",
+            dependencies: [],
+            path: "SmileLock"),
     ]
 )
