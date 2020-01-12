00cd2bf66dccebbc4e1f96a1ec3c3a89c8da5bdf
Martin Claesson <43297668+Claeysson@users.noreply.github.com>
2020-01-12 22:29:01 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Smile-Lock",
+    name: "SmileLock",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Smile-Lock",
-            targets: ["Smile-Lock"]),
+            name: "SmileLock",
+            targets: ["SmileLock"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Smile-Lock",
+            name: "SmileLock",
             dependencies: []),
         .testTarget(
-            name: "Smile-LockTests",
-            dependencies: ["Smile-Lock"]),
+            name: "SmileLockTests",
+            dependencies: ["SmileLock"]),
     ]
 )
